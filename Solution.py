5af0e22c2bbefec419997233250e5e1f6f48bb2c
Lucy Bychkov <lucybych@buffalo.edu>
2023-04-17 19:24:52 -0400

    Current solution for Problem2

--- Solution.py
+++ Solution.py
@@ -11,22 +11,69 @@ class Solution:
         self.isp = isp
         self.graph = graph
         self.info = info
-
+        
     def output_paths(self):
-        """
-        This method must be filled in by you. You may add other methods and subclasses as you see fit,
-        but they must remain within the Solution class.
-        """
-        paths, bandwidths, priorities = {}, {}, {}
-        # Note: You do not need to modify all of the above. For Problem 1, only the paths variable needs to be modified. If you do modify a variable you are not supposed to, you might notice different revenues outputted by the Driver locally since the autograder will ignore the variables not relevant for the problem.
-        # WARNING: DO NOT MODIFY THE LINE BELOW, OR BAD THINGS WILL HAPPEN
-
         isp = self.isp
         graph = self.graph
-        clients = self.info["list_clients"]
-        bandwidths = self.info["bandwidths"]
-
-        priority_queue = [(0, isp)]
-        visited = set()
-
+        list_clients = self.info["list_clients"]
+        bandwidthlist = self.info["bandwidths"]
+        bandwidthnum = {node: 0 for node in graph.keys()}
+        tolerances = self.info["alphas"]
+        paths, bandwidths, priorities = {}, {}, {}
+        graph_size = len(graph)
+        priors = [-1]*graph_size
+        search_queue = deque()
+        pathsfound = {node: False for node in list_clients}
+        layerfull = {}
+        allpathsfound = False
+        sortednodes = sorted(list_clients, key=lambda x: tolerances[x], reverse=True)
+        numpaths = 0
+        while(not allpathsfound):
+            search_queue.append(isp)
+            while search_queue:
+                node = search_queue.popleft()
+                for neighbor in graph[node]:
+                    if (priors[neighbor] == -1 and bandwidthnum[node] < bandwidthlist[node] and neighbor != isp):
+                        priors[neighbor] = node
+                        search_queue.append(neighbor)
+            bandwidthnum = {node: 0 for node in graph.keys()}
+            for client in list_clients:
+                if(not pathsfound[client]):
+                    path = []
+                    current_node = client
+                    while (current_node != -1):
+                        path.append(current_node)
+                        if(current_node != client):
+                            bandwidthnum[current_node] += 1
+                        current_node = priors[current_node]
+                    path = path[::-1]
+                    paths[client] = path
+                    pathsfound[client] = True
+                    numpaths += 1
+                else:
+                    for item in paths[client]:
+                        if(item != client):
+                            bandwidthnum[item] += 1
+            for node in bandwidthnum:
+                if bandwidthnum[node] > bandwidthlist[node]:
+                    for client in sortednodes:
+                        if node in paths[client] and node != client:
+                            for item in paths[client]:
+                                if item != node:
+                                    bandwidthnum[item] -= 1
+                            pathsfound[client] = False
+                            numpaths -= 1
+                            paths[client] = []
+                            bandwidthnum[node] -= 1
+                        bandwidthnum[isp] = sum([bandwidthnum[node] for node in graph[isp]])
+                        if bandwidthnum[node] <= bandwidthlist[node]:
+                            break
+            if(numpaths == len(list_clients)):
+                allpathsfound = True
+            else:
+                priors = [-1]*graph_size
+                for node in paths:
+                    i = 0
+                    while(i < len(paths[node]):
+                          
         return (paths, bandwidths, priorities)
