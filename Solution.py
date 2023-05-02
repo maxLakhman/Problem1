5929185844ad2023dc509117a1ddebea3d4e26fc
Lucy Bychkov <lucybych@buffalo.edu>
2023-05-02 16:41:40 -0400

    Base implemetation; need to figure out delay

--- Solution.py
+++ Solution.py
@@ -17,7 +17,86 @@ class Solution:
         This method must be filled in by you. You may add other methods and subclasses as you see fit,
         but they must remain within the Solution class.
         """
-        paths, bandwidths, priorities = {}, {}, {}
-        # Note: You do not need to modify all of the above. For Problem 1, only the paths variable needs to be modified. If you do modify a variable you are not supposed to, you might notice different revenues outputted by the Driver locally since the autograder will ignore the variables not relevant for the problem.
-        # WARNING: DO NOT MODIFY THE LINE BELOW, OR BAD THINGS WILL HAPPEN
+        graph = self.graph
+        isp = self.isp
+        list_clients = self.info["list_clients"]
+        simulator = Simulator()
+        bfspaths = simulator.local_bfs_path(graph, isp, list_clients)
+        originalbandwidths = self.info["bandwidths"]
+        bandwidths = originalbandwidths.copy()
+        bandwidthused = {node: 0 for node in graph.keys()}
+        tolerancea = self.info["alphas"]
+        toleranceb = self.info["betas"]
+        payments = self.info["payments"]
+        plawsuit = self.info["rho1"]
+        pfcc = self.info["rho2"]
+        fccval = self.info["is_fcc"]
+        fcclist = list()
+        for item in fccval:
+            if fccval[item] == 1:
+                fcclist.append(item)
+        costlawsuit = self.info["lawsuit"]
+        costfcc = self.info["fcc_fine"]
+        costbandwidth = self.info["cost_bandwidth"]
+        priors = [-1] * len(graph)
+        paths, priorities = {}, {}
+        #BFS
+        search_queue = deque()
+        search_queue.append(isp)
+        while search_queue:
+            node = search_queue.popleft()
+            for neighbor in graph[node]:
+                if (priors[neighbor] == -1 and neighbor != isp):
+                    priors[neighbor] = node
+                    search_queue.append(neighbor)
+        #Calculate paths
+        pathsthatusenode = {node: list() for node in graph.keys()}
+        for client in list_clients:
+            path = []
+            current_node = client
+            while (current_node != -1):
+                path.append(current_node)
+                if(current_node != client):
+                    bandwidthused[current_node] += 1
+                    pathsthatusenode[current_node].append(client)
+                current_node = priors[current_node]
+            path = path[::-1]
+            paths[client] = path
+        #Figure out delay for each client
+        complaintlist = list()
+        delay = {client: 0 for client in list_clients}
+        currentcycle = {node: 1 for node in graph.keys()}
+        for path in paths:
+            for item in paths[path]:
+                if(item != path):
+                    delay[path] += currentcycle[item]
+                    if ((bandwidthused[item] - currentcycle[item]) % bandwidths[item] == 0):
+                        currentcycle[item] += 1
+        print(delay)
+        #Find who will complain
+        complaintthreshold = {client: len(bfspaths[client]) * toleranceb[client] for client in list_clients}
+        for client in delay:
+            if delay[client] > complaintthreshold[client]:
+                complaintlist.append(client)
+        complaintlistsorted = sorted(complaintlist, key=lambda x: payments[x], reverse = True)
+        #Increase bandwidth for full nodes and for clients (lawsuit)
+        if len(complaintlist) >= len(list_clients) * plawsuit:
+            for client in complaintlistsorted:
+                i = 0
+                while delay[client] >= complaintthreshold[client] and i < len(paths[client]):
+                    if bandwidthused[paths[client][i]] > bandwidths[paths[client][i]] and paths[client][i] != client:
+                        bandwidths[paths[client][i]] += 1
+                        delay[client] -= 1
+                    i += 1
+                complaintlist.remove(client)
+        #Now calculated for fcc
+        if len(complaintlist) >= len(fcclist) * pfcc:
+            for client in complaintlistsorted:
+                i = 0
+                while delay[client] >= complaintthreshold[client] and i < len(paths[client]):
+                    if bandwidthused[paths[client][i]] > bandwidths[paths[client][i]] and paths[client][i] != client:
+                        bandwidths[paths[client][i]] += 1
+                        delay[client] -= 1
+                    i += 1
+                complaintlist.remove(client)
         return (paths, bandwidths, priorities)
